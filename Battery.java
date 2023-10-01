cffef5403eb67014fdcc04032a5660e521c5a3c6
srivastavautkarsh123 <143882134+srivastavautkarsh123@users.noreply.github.com>
2023-10-02 00:12:55 +0530

    Add files via upload

---
+++ Battery.java
@@ -0,0 +1,25 @@
+package com.example.myapplicationloc;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+import android.widget.Toast;
+
+public class Battery extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        // throw new UnsupportedOperationException("Not yet implemented");
+
+
+        Toast.makeText(context, "YOUR BATTERY IS LOW", Toast.LENGTH_LONG).show();
+
+    }
+
+    }
+
